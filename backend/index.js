fb23b76673c348929a7f81ff3cc10e1163cc3ea7
betulcetintas2706 <betul.cetintas@mail.utoronto.ca>
2026-03-07 11:13:10 -0500

    chore: push all project files and update ignore rules

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/webhook', require('./routes/whatsapp'));
+app.use('/api', require('./routes/api'));
 
-app.listen(3000, () => console.log('Roots backend running on 3000'));
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+});
+
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+});
+
+app.listen(3001, () => console.log('Roots backend running on 3001'));
